fb52e4bd1093154f68bd30f5ea3795648ef7fb02
simon <simon.schoeggler@hotmail.com>
2023-01-20 09:54:49 +0100

    categories api

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv';    
 
 dotenv.config();
 
@@ -9,6 +9,18 @@ const port = process.env.PORT;
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.get('/categories', (req: Request, res: Response) => {
+    getCategories().then(data => {
+        res.json(data);
+    });
+})
+
+async function getCategories(): Promise<any> {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+    const data = await response.json();
+    return data;
+}
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
