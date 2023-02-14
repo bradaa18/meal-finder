57461f424e8cca5f428671015cb581c186050347
simon <simon.schoeggler@hotmail.com>
2023-02-14 14:47:04 +0100

    comment

--- server/index.ts
+++ server/index.ts
@@ -6,10 +6,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+// request to the server
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+// request to the server /categories
 app.get('/categories', (req: Request, res: Response) => {
     getCategories().then(data => {
         console.log(data);
@@ -19,6 +21,7 @@ app.get('/categories', (req: Request, res: Response) => {
     });
 })
 
+// request to the server /meal/random
 app.get('/meal/random', (req: Request, res: Response) => {
     getRandomMeal().then(data => {
       console.log(data);
@@ -37,6 +40,7 @@ app.get('/category', (req: Request, res: Response) => {
     });
 });
 
+// request to /meal
 app.get('/meal', (req: Request, res: Response) => {
   getMeal(req.query.id + '').then(data => {
     console.log(data);
@@ -46,12 +50,14 @@ app.get('/meal', (req: Request, res: Response) => {
   });
 });
 
+// API call to get the meal by id
 async function getMeal(id: string): Promise<any> {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
     const data = await response.json();
     return data;
 }
 
+// API call to get the meal by category
 async function getCategory(name: string): Promise<any> {
   console.log(name);
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + name);
@@ -59,18 +65,21 @@ async function getCategory(name: string): Promise<any> {
     return data;
 }
 
+// API call to get the random meal
 async function getRandomMeal(): Promise<any> {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
     const data = await response.json();
     return data;
 }
 
+// API call to get the categories
 async function getCategories(): Promise<any> {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
     const data = await response.json();
     return data;
 }
 
+// start the Express server
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
